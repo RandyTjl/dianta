93c393bbb9bc2a6a3008513fd095545dddce56e5
RandyTang(唐佳力) <randytang@fih-foxconn.com.cn>
2018-09-03 16:13:36 +0800

    update

--- app/Http/Middleware/apiTokenAuthenticated.php
+++ app/Http/Middleware/apiTokenAuthenticated.php
@@ -4,8 +4,6 @@ namespace App\Http\Middleware;
 
 use App\Http\Controllers\Web\AuthController;
 use Closure;
-use Illuminate\Http\Request;
-
 
 
 class apiTokenAuthenticated
@@ -20,7 +18,6 @@ class apiTokenAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        $request = new Request();
         $apiToken = $request->header('apiToken');
         if(empty($apiToken)){
             return response()->json(['status'  => false, 'code'    => 300001, 'message' => config('apicode.code')[(int) 300001],]);
